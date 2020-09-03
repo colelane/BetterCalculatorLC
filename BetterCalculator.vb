a04550a81cbbb7908544bb256c3d8df091feb4aa
Lane Coleman <lane.coleman@live.com>
2020-09-02 20:24:31 -0600

    Formatting. Added comments.

--- BetterCalculator.vb
+++ BetterCalculator.vb
@@ -19,6 +19,7 @@ Module BetterCalculator
             Console.WriteLine("Enter q at any time to exit" & vbNewLine)
             Do
                 Try
+                    'The TryCatch is there to catch if any errors occur.  it 'tries' the inputs and if there is an error, the code moves to the catch area.
                     Console.WriteLine("Give me a number")
                     firstInput = Console.ReadLine()
                     firstNumber = CInt(firstInput)
@@ -27,24 +28,29 @@ Module BetterCalculator
                     secondNumber = CInt(secondInput)
                     goodData = True
                 Catch ex As Exception
+                    'exit must be added here after the exception. q is caught as an exception, then the if statement handles it.
                     If firstInput = "q" Or secondInput = "q" Then Exit Sub
                     Console.WriteLine("I asked for a number! Try again.")
                     goodData = False
+                    'goodData is a flag.  If there is a catch, the program loops and tries again to make it through without an error.
                 End Try
 
             Loop Until goodData = True
+            'loops until there is no catch
 
             Do
                 Console.WriteLine("1.add" & vbNewLine &
                                   "2.subtract" & vbNewLine &
                                   "3.multiply" & vbNewLine &
                                   "4.divide" & vbNewLine)
+                'vbnewline is a carriage return or enter. moves to the next line.
                 userResponse = Console.ReadLine()
                 If userResponse = "q" Then
+                    'could not put this in as an elseif, unsure why.
                     Exit Sub
 
                 ElseIf userResponse = "add" Or userResponse = "1" Then
-                Console.WriteLine(vbNewLine & firstNumber & " + " & secondNumber & " = " & firstNumber + secondNumber)
+                    Console.WriteLine(vbNewLine & firstNumber & " + " & secondNumber & " = " & firstNumber + secondNumber)
 
                 ElseIf userResponse = "subtract" Or userResponse = "2" Then
                     Console.WriteLine(vbNewLine & firstNumber & " - " & secondNumber & " = " & firstNumber - secondNumber)
@@ -70,10 +76,19 @@ Module BetterCalculator
 
                 End If
 
-            Loop Until userResponse = "add" Or userResponse = "multiply" Or userResponse = "divide" Or userResponse = "subtract" Or userResponse = "1" Or userResponse = "2" Or userResponse = "3" Or userResponse = "4"
+            Loop Until userResponse = "add" _
+                    Or userResponse = "multiply" _
+                    Or userResponse = "divide" _
+                    Or userResponse = "subtract" _
+                    Or userResponse = "1" _
+                    Or userResponse = "2" _
+                    Or userResponse = "3" _
+                    Or userResponse = "4"
             Console.ReadLine()
             Console.Clear()
         Loop
+        'clears the console and starts over so the calculator can be used again.
+
     End Sub
 
 End Module
